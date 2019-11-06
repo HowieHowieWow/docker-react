a9dc2c36cdcdd09e6c7a4909f8a454c8aabe757c
Haoran Wang <36777545+HowieHowieWow@users.noreply.github.com>
2019-11-06 11:01:12 -0600

    comment added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi There! Changed!! Again!!! New Feature Added!!!!
+          Hi There! Changed!! Again!!! New Feature Added!!!! // added
         </p>
         <a
           className="App-link"
